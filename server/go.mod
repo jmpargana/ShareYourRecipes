30dd22c5d6454caab1e372f6ef799f1cbfbc1e15
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-12-04 01:27:02 +0100

    travis support for testing

--- server/go.mod
+++ server/go.mod
@@ -3,11 +3,6 @@ module server
 go 1.15
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/google/uuid v1.1.1
-	github.com/moemoe89/go-unit-test-sql v0.0.0-20200619083906-541278f731fe
-	github.com/stretchr/testify v1.6.1
-	gorm.io/driver/mysql v1.0.3
-	gorm.io/gorm v1.20.7
+	github.com/ory/dockertest/v3 v3.6.2
 )
--- server/main.go
+++ server/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "server/db"
+import (
+	"os"
+	"server/db"
+)
+
+var (
+	user     = os.Getenv("MYSQL_ROOT_USERNAME")
+	password = os.Getenv("MYSQL_ROOT_PASSWORD")
+	endPoint = os.Getenv("ENDPOINT")
+	database = os.Getenv("DATABASE")
+)
 
 func main() {
-	_, err := db.Setup()
+	_, err := db.New("mysql", db.GenerateDSN(user, password, endPoint, database), 0, 0)
 	if err != nil {
 		panic(err)
 	}
