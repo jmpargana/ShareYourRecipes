95034b7a916012d411c60de2816af0a6bd9d33f6
jmpargana <jmpargana@stud.uni-frankfurt.de>
2020-12-04 14:46:00 +0100

    proper travis

--- server/db/helpers_test.go
+++ server/db/helpers_test.go
@@ -1,33 +1,9 @@
 package db
 
-// func setupMock() (*DBWrapper, sqlmock.Sqlmock, func()) {
-// 	db, mock := NewMock()
-// 	w := &DBWrapper{db}
-// 	return w, mock, func() {
-// 		w.Close()
-// 	}
-// }
+import "testing"
 
-// func compareRecipes(t *testing.T, lhs, rhs *models.Recipe) {
-// 	if !lhs.Equals(rhs) {
-// 		t.Fatalf("expected %v, got %v", rhs, lhs)
-// 	}
-// }
-
-// func checkNil(t *testing.T, err error) {
-// 	if err != nil {
-// 		t.Fatalf("shouldn't receive error, got: %s", err)
-// 	}
-// }
-
-// func recipeNotNil(t *testing.T, recipe *models.Recipe) {
-// 	if recipe != nil {
-// 		t.Fatalf("shouldn't find anything, %v", recipe)
-// 	}
-// }
-
-// func shouldntBeNil(t *testing.T, err error) {
-// 	if err == nil {
-// 		t.Fatalf("should have failed!")
-// 	}
-// }
+func assertNil(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
--- server/db/tag_test.go
+++ server/db/tag_test.go
@@ -1,23 +1,61 @@
 package db
 
 import (
+	"context"
+	"log"
 	"server/models"
 	"testing"
+	"time"
 )
 
 func TestTagInsertAndFind(t *testing.T) {
 	var tag models.Tag = "curry"
 
-	if err := w.InsertTag(0, tag); err != nil {
-		t.Fatal(err)
-	}
+	assertNil(t, w.InsertTag(0, tag))
 
 	id, err := w.FindTag(tag)
-	if err != nil {
-		t.Fatal(err)
-	}
+	assertNil(t, err)
 
 	if id != 1 {
 		t.Fatalf("shoul have received first id, instead got: %d", id)
 	}
+
+	// Insert Same Tag again and check
+	assertNil(t, w.InsertTag(1, tag))
+
+	id, err = w.FindTag(tag)
+	assertNil(t, err)
+
+	if id != 1 {
+		t.Fatalf("shoul have received first id, instead got: %d", id)
+	}
+
+	// Check if two recipe_tags where created
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	stmt, err := w.db.PrepareContext(ctx, "SELECT recipeid FROM recipetags where tagid = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := stmt.Query(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	counter := 0
+	for rows.Next() {
+		var recipeid int
+		rows.Scan(&recipeid)
+		counter++
+	}
+
+	if counter != 2 {
+		t.Fatal("should receive 2")
+	}
+}
+
+func TestInsertionMultipleTagsWithDuplicates(t *testing.T) {
+
 }
